3a774f2c7caf4afb95a2ab8ec7f688fbe265d62d
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-07-30 09:51:38 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,10 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
+
+  def create
+    @message = Message.new
 end
